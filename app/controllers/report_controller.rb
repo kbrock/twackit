28b074c2ab2cf5736722f5e6174d69bc6dd520ab
Jeremy Weiskotten <jweiskotten@patientslikeme.com>
2009-04-18 16:25:21 -0400

    Screenshot

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,7 +1,7 @@
 class ReportController < ApplicationController
   def show
-    @twitter_username = params[:twitter_username]
-    @hashtag = params[:hashtag]
+    @twitter_username = params[:twitter_username].strip
+    @hashtag = params[:hashtag].gsub('#', '').strip
     @report = Tweet.report(
         :twitter_username => @twitter_username, 
         :hashtag => @hashtag
