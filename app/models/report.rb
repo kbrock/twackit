9bd6d67ca6e41f40f17004f7231deef33d7f0f10
Jeremy Weiskotten <jweiskotten@patientslikeme.com>
2009-04-19 17:07:51 -0400

    Show twitter profile picture, full name on report page

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,6 @@
 class Report
-  attr_reader :tweets, :twitter_username, :hashtag, :title, :values
+  attr_reader :tweets, :twitter_username, :hashtag, :title, :values, 
+      :full_name, :picture_url  
   
   def initialize(attribs)
     @hashtag = attribs[:hashtag]
@@ -8,7 +9,21 @@ class Report
 
     @tweets = Tweet.for_report(self.twitter_username, self.hashtag)
     @values = self.tweets.map { |t| t.value }
+
+    # TODO Using this to fetch things from the API like photo URL and full name. Cache this stuff?
+    twitter_user = Twitter.user(@twitter_username)
+    if twitter_user
+      @full_name = twitter_user.name
+      @picture_url = twitter_user.profile_image_url
+    else
+      @full_name = @twitter_username
+      @picture_url = nil
+    end    
   end  
+  
+  def picture_description
+    "#{twitter_username}'s Twitter profile picture"
+  end
   
   def tweets_by_date
     @tweets_by_date ||= @tweets.group_by { |t| t.status_at.to_date }
