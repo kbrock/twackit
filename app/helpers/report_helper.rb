c9aeaee46ecd2996f93b91785cc4774fc7319925
Jeremy Weiskotten <jweiskotten@patientslikeme.com>
2009-04-21 15:43:52 -0400

    Added missing </li>

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -30,7 +30,7 @@ module ReportHelper
     
     calendar(:year => date.year, :month => date.month, :show_today => true, :abbrev => (0..0)) do |d|
       tweets = tweets_by_date[d]
-      values = '<ul class="values"><li>' + tweets.map { |t| link_to_tweet(t) }.join('</li><li>') + '</ul>' unless tweets.blank?
+      values = '<ul class="values"><li>' + tweets.map { |t| link_to_tweet(t) }.join('</li><li>') + '</li></ul>' unless tweets.blank?
       ["<span>#{d.mday}</span>#{values}"]
     end
   end
