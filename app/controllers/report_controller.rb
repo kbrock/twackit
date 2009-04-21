66f67cb26bdbcd7c54eddae3fd442b6cfe39d121
Jeremy Weiskotten <jweiskotten@patientslikeme.com>
2009-04-21 12:21:01 -0400

    redirect to clean url

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,6 +3,12 @@ class ReportController < ApplicationController
     @twitter_username = params[:twitter_username].strip
     @hashtag = params[:hashtag].gsub('#', '').strip
     
+    if @hashtag != params[:hashtag] || @twitter_username != params[:twitter_username]
+      # clean up the URL
+      redirect_to report_path(:twitter_username => @twitter_username, :hashtag => @hashtag), :status => :moved_permanently
+      return
+    end
+    
     # redirect_to root_url and return if @twitter_username.blank? || @hashtag.blank?
     
     @report = Tweet.report :twitter_username => @twitter_username, :hashtag => @hashtag
