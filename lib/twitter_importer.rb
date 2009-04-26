0b9a727d1e1c45b97a1d69ea79479cbdcab5c2ff
Jeremy Weiskotten <jweiskotten@patientslikeme.com>
2009-04-26 11:53:14 -0400

    Importer refactoring

--- lib/twitter_importer.rb
+++ lib/twitter_importer.rb
@@ -1,64 +1,100 @@
 # Uses Twitter gem. http://github.com/jnunemaker/twitter/tree/master
 class TwitterImporter
 
-  def self.import!(per_page=100)
-    t0 = Time.now
-    results = []
-    since_id = Tweet.latest_id
+  class << self
+    
+    def import!(per_page=100)
+      import_logging do
+        since_id = Tweet.latest_id
 
-    Rails.logger.info "Searching for new tweets since #{since_id}"
+        Rails.logger.info "Searching for new tweets since #{since_id}"
 
-    # fetch all new tweets
-    page = 1
-    while true
-      Rails.logger.info "Fetching page #{page} (#{per_page} per page)"
-      
-      # find tweets on this page
-      page_results = searcher.since(since_id).per_page(per_page).page(page).fetch.results      
-      results += page_results
+        # fetch all new tweets
+        searcher = twitter_searcher.to(TWITTER_ID).since(since_id).per_page(per_page)
+        search_results = fetch_pages(per_page) do |page|
+          searcher.page(page).fetch.results
+        end
 
-      # if we didn't get a full page, we're at the end
-      break if page_results.size < per_page
-      
-      page += 1
-    end
+        # TODO: Fetch direct messages?
 
-    Rails.logger.info "Found #{results.size} new tweets"
-
-    # TODO: Fetch direct messages?
-
-
-    # log new tweets to database
-    tweets = Array.new(results.size)
-    errors = 0
-    results.each_with_index do |r, i|
-      tweet = Tweet.build_for_status(r)
-
-      # Don't fail too noisily... we want to continue processing!
-      if tweet.save
-        tweets[i] = tweet
-      else
-        # TODO send error to hoptoad/exceptional?
-        Rails.logger.error "Failed to save tweet: #{tweet.errors.full_messages.join('.')}"
-        errors += 1
+        # write new tweets to database
+        create_tweets(search_results) { |r| Tweet.build_for_status(r) }
       end
     end
-    
-    # log some stats about this import
-    duration = Time.now - t0
-    distinct_users = tweets.map { |t| t.from_user }.uniq.size
-    Import.create! :tweets => results.size, 
-        :distinct_users => distinct_users,
-        :errs => errors, :duration => duration
-    
-    tweets
-  end
-  
-  protected
-  
-    def self.searcher
-      Twitter::Search.new.to(TWITTER_ID)
+
+    def retro_import!(per_page=100, username, hashtag)
+      import_logging do
+        searcher = Twitter::Search.new.per_page(per_page).from(username).hashed(hashtag)
+        search_results = fetch_pages(per_page) do |page|
+          searcher.page(page).fetch.results
+        end
+        
+        create_tweets(search_results) do |r|
+          # custom parsing for pre-existing tweets
+          tweet = Tweet.new
+        end
+      end
     end
+
+    
+  protected
+    def twitter_searcher
+      Twitter::Search.new
+    end
+    
+    def create_tweets(search_results)
+      tweets = Array.new(search_results.size)
+      search_results.each_with_index do |r, i|
+        tweet = yield r
+
+        # Don't fail too noisily... we want to continue processing!
+        if tweet.save
+          tweets[i] = tweet
+        else
+          # TODO send error to hoptoad/exceptional?
+          Rails.logger.error "Failed to save tweet: #{tweet.errors.full_messages.join('.')}"
+        end
+      end
+      
+      tweets
+    end
+    
+    def import_logging
+      tweets_imported, duration = timer { yield }
+      
+      # log some stats about this import
+      distinct_users = tweets_imported.map { |t| t.from_user }.uniq.size
+      tweet_count = tweets_imported.compact.size # the number of non-nil entries
+      error_count = tweets_imported.size - tweet_count # the number of nil entries
+    
+      Import.create! :tweets => tweet_count, :errs => error_count, 
+          :distinct_users => distinct_users, :duration => duration    
+    end
+    
+    def timer
+      t0 = Time.now    
+      yield, Time.now - t0
+    end
+
+    def fetch_pages(per_page)
+      search_results = []
+      page = 1
+
+      while true
+        Rails.logger.info "Fetching page #{page} (#{per_page} per page)"
+
+        page_results = yield page
+        search_results += page_results      
+
+        # if we didn't get a full page, we're at the end
+        break if page_results.size < per_page
+        page += 1
+      end    
+
+      Rails.logger.info "Found #{search_results.size} new tweets"
+      search_results
+    end
+  end
   
   
 end
