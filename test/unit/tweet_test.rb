0a73062164075ce09fbd5e8cb581c18028821985
Jeremy Weiskotten <jweiskotten@patientslikeme.com>
2009-05-05 09:01:39 -0400

    design tweaks

--- test/unit/tweet_test.rb
+++ test/unit/tweet_test.rb
@@ -2,7 +2,6 @@ require File.dirname(__FILE__) + '/../test_helper'
 require 'ostruct'
 
 class TweetTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
   test "build_for_status and save" do
     # mock a status objects returned by the Twitter search API 
     status = OpenStruct.new(
@@ -22,5 +21,26 @@ class TweetTest < ActiveSupport::TestCase
     assert_equal 'this is a note', tweet.note
     assert_equal '175', tweet.data
     assert_equal ['weight', 'tag2'], tweet.hashtags.map(&:value)
+  end
+  
+  test "with spaces" do
+    # mock a status objects returned by the Twitter search API 
+    status = OpenStruct.new(
+        :created_at => Time.utc(2009, 4, 29, 19, 6, 33),
+        :from_user => 'doctorzaius',
+        :status_text => '@twackit  180            #calories  Banana Pudding Ice Cream',
+        :iso_language_code => 'en')
+
+    tweet = Tweet.build_for_status status    
+    tweet.save!
+
+    assert tweet.processed?
+    assert_equal 'doctorzaius', tweet.from_user
+    assert_equal 'en', tweet.language
+
+    # this stuff gets parsed from status_text
+    assert_equal 'Banana Pudding Ice Cream', tweet.note
+    assert_equal '180', tweet.data
+    assert_equal ['calories'], tweet.hashtags.map(&:value)
   end
 end
