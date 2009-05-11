80daf13c4b00c4dc03d9a9581282ffb824e0adc5
Jeremy Weiskotten <jweiskotten@patientslikeme.com>
2009-05-10 21:29:35 -0400

    Added 'tweet this page' with random prompt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,26 @@ module ApplicationHelper
     'production' == RAILS_ENV
   end
   
+  def tweet_this_page(locals={})
+    locals[:status] ||= 'Whatever it is, track it with Twackit! http://twackit.com'
+    render :partial => 'layouts/tweet_this_page', :locals => locals
+  end
+  
+  def random_tweet_prompt
+    [
+    'Make the bird happy.',
+    'Make birdy happy.',
+    'The bird is watching.',
+    'Love the bird.',
+    'Be excellent.',
+    'Be cool.',
+    'Spread the love.',
+    'Tell your friends.',
+    'Birdy loves you.',
+    'Impress the bird.'
+    ].rand    
+  end
+  
   def feedback_page
     { :href => 'http://twackit.uservoice.com' }
   end
