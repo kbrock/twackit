7393e6fad0bc85aeaa5d30490db9ec33a3598f7d
Keenan Brock <kbrock@patientslikeme.com>
2011-04-26 00:41:23 -0400

    explicitly declare tweet data to be nullable

--- db/migrate/20110326142907_create_float_data_column.rb
+++ db/migrate/20110326142907_create_float_data_column.rb
@@ -1,6 +1,8 @@
 class CreateFloatDataColumn < ActiveRecord::Migration
   def self.up
-    add_column :tweets, :float_data, :float
+    # if it is non numeric data, have it be null (being explicit here)
+    add_column :tweets, :float_data, :float, :null => true
+
     if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
       execute "update tweets set float_data = cast(data as float) where data similar to '^[.0-9]+$'"
     else
