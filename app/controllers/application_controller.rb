7a37ffcffa7f2766a40bfae4b6b24472bab5cfb6
Keenan Brock <kbrock@patientslikeme.com>
2011-06-11 15:48:57 -0400

    moved logging out of the controller into the log model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,13 +24,6 @@ protected
 
 
   def log_request
-    PageRequest.create :url => request.url,
-        :method => request.method.id2name,
-        :user_agent => request.user_agent,
-        :remote_ip => request.remote_ip,
-        :referrer => request.referrer,
-        :xhr => request.xhr?,
-        :session_id => session[:session_id],
-        :cookies => cookies.inspect
+    PageRequest.log(session[:session_id],cookies, request)
   end
 end
--- app/models/page_request.rb
+++ app/models/page_request.rb
@@ -1,2 +1,13 @@
 class PageRequest < ActiveRecord::Base
+
+  def self.log(session_id, cookies, request)
+    create :url => request.url,
+      :method => request.method.id2name,
+      :user_agent => request.user_agent,
+      :remote_ip => request.remote_ip,
+      :referrer => request.referrer,
+      :xhr => request.xhr?,
+      :session_id => session_id,
+      :cookies => cookies.inspect
+  end
 end
