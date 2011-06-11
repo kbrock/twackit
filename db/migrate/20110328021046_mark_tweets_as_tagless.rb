d05d17c44f5eb8ee23f87340c7705836bdee0702
Keenan Brock <kbrock@patientslikeme.com>
2011-06-11 15:39:24 -0400

    update migration so it works in sqlite3 and postgres

--- db/migrate/20110328021046_mark_tweets_as_tagless.rb
+++ db/migrate/20110328021046_mark_tweets_as_tagless.rb
@@ -1,10 +1,11 @@
 class MarkTweetsAsTagless < ActiveRecord::Migration
   def self.up
     add_column :tweets, :tagless, :boolean, :default => false, :null => false
-    
+
+    # set to true (but using # for sqlite support)
     execute %{
       update tweets
-      set tagless = true
+      set tagless = 1
       where not exists (select 1 from tweet_tags where tweet_id = tweets.id)
     }
   end
